611b9fa6699a4c099b0fdefc38c763caaf39b2dd
Andrei Tudorica <andreitudoricacj@gmail.com>
2019-05-14 20:12:30 +0300

    Live Traffic Server

---
+++ TCCC/LiveTrafficServer/LiveTrafficServer/Controllers/ValuesController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Itinero;
+using Itinero.IO.Osm;
+using Itinero.LocalGeo;
+using Itinero.Osm.Vehicles;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiveTrafficServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var routerDb = new RouterDb();
+
+            using (var stream = System.IO.File.OpenRead("D:\\Andrei\\Scoala\\LICENTA\\Maps\\Cluj-Napoca.pbf"))
+            {
+                routerDb.LoadOsmData(stream, Vehicle.Car);
+            }
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
