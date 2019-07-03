f299deeaa5e12936f63c3fb0f5e43ff2a507e79a
Andrei Tudorica <andreitudoricacj@gmail.com>
2019-07-04 00:34:00 +0300

    to laptop

---
+++ TCCC/TrafficSimulator/TrafficSimulator/Models/StatisticsEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficSimulator.Models
+{
+    public class StatisticsEntry
+    {
+        public TimeSpan totalTime;
+        public TimeSpan averageTime;
+        public double averageTouchedFeatures;
+        public double averageTouchedSegments;
+        public double averageSpeed;
+        public TimeSpan totalUpdateWaitTime;
+        public TimeSpan totalRouteRequestWaitTime;
+        public TimeSpan averageUpdateWaitTime;
+        public TimeSpan averageRouteRequestWaitTime;
+        public TimeSpan averageTimeWithoutWaiting;
+        public int threshold;
+    }
+}
